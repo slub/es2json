27a323f386af0a4be35cf42d5ac19e63706c6af3
Bernhard Hering <bernhard.hering@slub-dresden.de>
2020-11-12 14:24:37 +0100

    added more tests for query with idfile handling

--- tests/test_es2json_esfunctions.py
+++ tests/test_es2json_esfunctions.py
@@ -93,10 +93,12 @@ def test_esgenerator_source_excludes():
 
 def test_esgenerator_query_headless():
     query = {"query": {"match": {"baz.keyword": "test666"}}}
+    enter = False
     with es2json.ESGenerator(body=query, headless=True, **default_kwargs) as es:
         for record in es.generator():
+            enter = True
             assert record == {"foo": 666, "bar": MAX-666, "baz": "test666"}
-
+    assert enter  # testing if we even entered the generator() at all...useful for tests where we assert directly when iterating over the generator
 
 def test_esidfilegenerator_iterable():
     expected_records = []
@@ -138,7 +140,7 @@ def test_eidfileconsumegenerator():
     fd = str(uuid.uuid4())
     expected_records = []
     with open(fd, "w") as outp:
-        for n in range(200, 300):
+        for n in range(MAX-200, MAX-100):
             retrecord = {}
             retrecord["foo"] = n
             retrecord["baz"] = "test{}".format(n)
@@ -147,6 +149,27 @@ def test_eidfileconsumegenerator():
             print(n, file=outp)
     records = []
     with es2json.IDFileConsume(idfile=fd, headless=True, **default_kwargs) as es:
+        for record in es.generator():
+            records.append(dict(sorted(record.items())))
+    assert sorted(expected_records, key=lambda k: k["foo"]) == sorted(records, key=lambda k: k["foo"])
+    assert es2json.isfile(fd) is False
+
+
+def test_eidfileconsumegenerator_query():
+    fd = str(uuid.uuid4())
+    expected_records = []
+    with open(fd, "w") as outp:
+        for n in range(MAX-200, MAX-100):
+            if str(n).startswith('9'):
+                retrecord = {}
+                retrecord["foo"] = n
+                retrecord["baz"] = "test{}".format(n)
+                retrecord["bar"] = MAX-n
+                expected_records.append(dict(sorted(retrecord.items())))
+                print(n, file=outp)
+    query = {"query": {"prefix":  {"baz": "test9"}}}
+    records = []
+    with es2json.IDFileConsume(idfile=fd, headless=True, body=query, **default_kwargs) as es:
         for record in es.generator():
             records.append(dict(sorted(record.items())))
     assert sorted(expected_records, key=lambda k: k["foo"]) == sorted(records, key=lambda k: k["foo"])
@@ -166,10 +189,40 @@ def test_eidfileconsumegenerator_missing_ids():
                 retrecord["baz"] = "test{}".format(n)
                 retrecord["bar"] = MAX-n
                 expected_records.append(dict(sorted(retrecord.items())))
+    enter = False
     with es2json.IDFileConsume(idfile=fd, headless=False, **default_kwargs) as es:
         for record in es.generator():
+            enter = True
             found_ids.add(record["_id"])
             assert dict(sorted(record["_source"].items())) in expected_records
+    assert enter
+    with open(fd, "r") as inp:
+        for ppn in inp:
+            assert ppn.rstrip() not in found_ids
+    os.remove(fd)  # cleanup
+
+
+def test_eidfileconsumegenerator_missing_ids_query():
+    fd = str(uuid.uuid4())
+    expected_records = []
+    found_ids = set()
+    with open(fd, "w") as outp:
+        for n in range(MAX-200, MAX+200):
+            print(n, file=outp)
+            if n < MAX and str(n).startswith("9"):
+                retrecord = {}
+                retrecord["foo"] = n
+                retrecord["baz"] = "test{}".format(n)
+                retrecord["bar"] = MAX-n
+                expected_records.append(dict(sorted(retrecord.items())))
+    query = {"query": {"prefix":  {"baz": "test9"}}}
+    enter = False
+    with es2json.IDFileConsume(idfile=fd, headless=False, body=query, **default_kwargs) as es:
+        for record in es.generator():
+            enter = True
+            found_ids.add(record["_id"])
+            assert dict(sorted(record["_source"].items())) in expected_records
+    assert enter
     with open(fd, "r") as inp:
         for ppn in inp:
             assert ppn.rstrip() not in found_ids
