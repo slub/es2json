8873e45cbac53bdc376a9dbcfbbc94e9f72dbb4b
Bernhard Hering <bernhard.hering@slub-dresden.de>
2020-11-13 08:41:20 +0100

    don't generate testdata just because some import

--- tests/generate_testdata.py
+++ tests/generate_testdata.py
@@ -4,9 +4,10 @@ import gzip
 
 MAX = 1000
 
-with gzip.open("testdata.ldj.gz", "wt") as outp:
-    for n in range(0, MAX):
-        print(json.dumps({"foo": n,
-                          "bar": MAX-n,
-                          "baz": "test{}".format(n)
-                          }), file=outp)
+if __name__ == "__main__":
+    with gzip.open("testdata.ldj.gz", "wt") as outp:
+        for n in range(0, MAX):
+            print(json.dumps({"foo": n,
+                            "bar": MAX-n,
+                            "baz": "test{}".format(n)
+                            }), file=outp)
