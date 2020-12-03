af5a428a37ce0c4c7edc0205e2801aec0e713479
Tom Schilling <Tom.Schilling@slub-dresden.de>
2020-12-03 16:19:34 +0100

    changed cli: use -server as only config option

--- es2json/cli.py
+++ es2json/cli.py
@@ -12,58 +12,57 @@ def run():
     """
     here We build a simple cmdline tool so we can use our classes from shell
     """
-    parser = argparse.ArgumentParser(description='Elasticsearch to JSON')
-    parser.add_argument('-host', type=str, default="127.0.0.1",
-                        help='hostname or ip of the ElasticSearch-node to use,'
-                        ' default: localhost.')
-    parser.add_argument('-port', type=int, default=9200,
-                        help='Port of the ElasticSearch-node to use, default: 9200.')
-    parser.add_argument('-index', type=str,
-                        help='ElasticSearch Search Index to use')
-    parser.add_argument('-type', type=str, dest="type_",
-                        help='ElasticSearch Search Index Type to use')
-    parser.add_argument('-server', type=str, help="use http://host:port/index/type/id?pretty. "
-                        "overwrites host/port/index/id/pretty")
+    parser = argparse.ArgumentParser(description='Query elasticsearch indices/index/documents and print them '
+                                                 'formatted as JSON-Objects',
+                                     formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('-server', type=str, help="use http://host:port/index/type/id.\n"
+                        "host:port - hostname or IP with port of the elasticsearch node to query\n"
+                        "            default: localhost:9200\n"
+                        "index     - index to query\n"
+                        "            default: None → queries across all available indices\n"
+                        "type      - elasticsearch doctype to use (optional)\n"
+                        "id        - identifier of one specific document to query (optional)",
+                        default="http://127.0.0.1:9200")
     parser.add_argument('-source', type=helperscripts.str2bool, nargs='?',
                         const=True, default=True,
                         help='return the Document or just the Elasticsearch-Metadata')
     parser.add_argument('-size', type=str, default=None,
-                        help='just return the first n-Records of the search, '
-                        'or return a python slice, e.g. 2:10 returns a list '
-                        'from the 2nd including the 9th element of the search '
-                        'only works with the ESGenerator')
+                        help='just return the first n-Records of the search,\n'
+                        'or return a python slice, e.g. 2:10 returns a list\n'
+                        'from the 2nd including the 9th element of the search\n'
+                        'only works with the ESGenerator\n'
+                        'Note: Not all slice variants may be supported')
     parser.add_argument('-timeout', type=int, default=10,
-                        help='Set the time in seconds after when a ReadTimeoutError can occur. Default is 10 seconds. Raise for big/difficult querys ')
+                        help='Set the time in seconds after when a ReadTimeoutError can occur.\n'
+                        'Default is 10 seconds. Raise for big/difficult querys ')
     parser.add_argument("-includes", type=str,
                         help="just include following _source field(s) in the _source object")
     parser.add_argument("-excludes", type=str,
                         help="exclude following _source field(s) from the _source object")
-    parser.add_argument(
-        "-id", type=str, help="retrieve single document (optional)")
-    parser.add_argument("-headless", action='store_true', default=False, help="don't print Elasticsearch metadata")
+    parser.add_argument("-headless", action='store_true', default=False,
+                        help="don't print Elasticsearch metadata")
     parser.add_argument('-body', type=helperscripts.jsonstring_or_file, help='Searchbody')
     parser.add_argument(
         '-idfile', type=str, help="path to a file with \\n-delimited IDs to process")
     parser.add_argument('-idfile_consume', type=str,
                         help="path to a file with \\n-delimited IDs to process")
-    parser.add_argument('-pretty', action='store_true', default=False, help="prettyprint")
-    parser.add_argument('-verbose', action='store_true', default=True, help="print progress for large dumps")
+    parser.add_argument('-pretty', action='store_true', default=False,
+                        help="prettyprint the json output")
+    parser.add_argument('-verbose', action='store_true', default=True,
+                        help="print progress for large dumps")
     parser.add_argument('-chunksize', type=int, default=1000,
                         help="chunksize of the search window to use")
     args = parser.parse_args()
-    if args.server:                                 # http://server.de:1234/index/_doc/101?pretty
-        slashsplit = args.server.split("/")         # → [http:, , server.de:1234, index, _doc, 101?pretty]
-        args.host = slashsplit[2].rsplit(":")[0]
-        args.port = int(args.server.split(":")[2].rsplit("/")[0]) # raise Error if port not castable to int
-        args.index = args.server.split("/")[3]
-        if len(slashsplit) > 4:
-            args.type = slashsplit[4]
-        if len(slashsplit) > 5:
-            if "?pretty" in args.server:
-                args.pretty = True
-                args.id = slashsplit[5].rsplit("?")[0]
-            else:
-                args.id = slashsplit[5]
+    #parsing server                             # http://server.de:1234/index/_doc/101
+    slashsplit = args.server.split("/")         # → [http:, , server.de:1234, index, _doc, 101]
+    args.host = slashsplit[2].rsplit(":")[0]
+    args.port = int(args.server.split(":")[2].rsplit("/")[0]) # raise Error if port not castable to int
+    if len(slashsplit) > 3:
+        args.index = slashsplit[3]
+    if len(slashsplit) > 4:
+        args.type_ = slashsplit[4]
+    if len(slashsplit) > 5:
+        args.id_ = slashsplit[5]
     if args.size:
         """
         we build the slice() object here, if this fails because of user input,
@@ -73,9 +72,9 @@ def run():
         if isinstance(args.size, int):  # oh, we got an single number, not a string with an number or even an string describing a slice
             args.size = str(args.size)
         if ':' in args.size:
-            args.slice = slice(int(args.size.split(':')[0]), int(args.size.split(':')[1]), 1)
+            args.slice_ = slice(int(args.size.split(':')[0]), int(args.size.split(':')[1]), 1)
         else:
-            args.slice = slice(0, int(args.size), 1)
+            args.slice_ = slice(0, int(args.size), 1)
     if args.headless and not args.source:
         helperscripts.eprint("ERROR! do not use -headless and -source False at the same Time!")
         exit(-1)
